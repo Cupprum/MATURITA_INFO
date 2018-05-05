72ec70facbf7adadfe5af09477f07a10299441de
Cupprum <branisa.samuel@windowslive.com>
2018-05-05 19:40:35 +0200

    app3_2.py works

---
+++ app3_2.py
@@ -0,0 +1,24 @@
+txt = open('../ZADANIA/Zadanie_2_Uloha_1.txt', 'r').readlines()
+code = open('../ZADANIA/Zadanie_2_Maska_1.txt', 'r').readlines()
+
+txt_list = []
+final_list = []
+code_list = []
+
+for x in code:
+    hlp = [int(y) for y in x.split()]
+    code_list.append(hlp)
+
+for x in txt:
+    print(x)
+    txt_list.append(x)
+
+counter = 0
+for x in code_list:
+    for y in x:
+        txt_list[counter] = (
+            txt_list[counter][:y - 1] + " " + txt_list[counter][y - 1:])
+    counter += 1
+
+for x in txt_list:
+    print(x)
