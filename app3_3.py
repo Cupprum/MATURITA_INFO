8b27e3b780da80beb11c22156ee66d9aa0e82b51
Cupprum <branisa.samuel@windowslive.com>
2018-05-05 20:15:52 +0200

    app3_3.py works fine

--- app3_3.py
+++ app3_3.py
@@ -2,29 +2,22 @@ txt = open('../ZADANIA/Zadanie_3_Uloha_1.txt', 'r').readlines()
 
 day_list = []
 other_list = []
+stat_list = ['BB', 'BZ', 'ZB', 'ZZ']
 
 for x in range(0, len(txt), 2):
     day_list.append(txt[x][:-1])
     other_list.append(txt[x + 1][:-1])
 
 for x in range(len(day_list)):
-    counterBB = 0
-    counterBZ = 0
-    counterZB = 0
-    counterZZ = 0
+    dic_counters = {'counterBB': 0,
+                    'counterBZ': 0,
+                    'counterZB': 0,
+                    'counterZZ': 0}
 
     for y in range(int(day_list[x]) - 1):
         akt = other_list[x]
-        if akt[y] == 'B' and akt[y + 1] == 'B':
-            counterBB += 1
-        if akt[y] == 'B' and akt[y + 1] == 'Z':
-            counterBZ += 1
-        if akt[y] == 'Z' and akt[y + 1] == 'B':
-            counterZB += 1
-        if akt[y] == 'Z' and akt[y + 1] == 'Z':
-            counterZZ += 1
-    print(f"BB {counterBB}")
-    print(f"BZ {counterBZ}")
-    print(f"ZB {counterZB}")
-    print(f"ZZ {counterZZ}")
-    print('\n')
+        for z in stat_list:
+            if akt[y] == z[0] and akt[y + 1] == z[1]:
+                dic_counters['counter' + z] += 1
+
+    print(dic_counters)
