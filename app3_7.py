8c95d80b0cc518f50dd530ab635dccb10c6d5bb7
Cupprum <branisa.samuel@windowslive.com>
2018-05-09 21:32:20 +0200

    stores everything in dictionary of matrixes

---
+++ app3_7.py
@@ -0,0 +1,37 @@
+txt = open('../ZADANIA/Zadanie_7_Uloha_1.txt', 'r').readlines()
+
+list_square = []
+list_actual_numbers = []
+
+for x in range(0, len(txt) - 1, 2):
+    list_square.append(txt[x][:-1])
+    list_actual_numbers.append(txt[x + 1][:-1])
+
+
+list_square = list(map(int, list_square))
+
+dic_final = {}
+counter = 1
+
+for x in range(len(list_square)):
+    temporary1 = list_actual_numbers[x].split(" ")
+    temporary1 = list(map(int, temporary1))
+
+    square = list_square[x] * list_square[x]
+    list_for_push = []
+
+    for y in range(0, square, list_square[x]):
+        temporary2 = []
+
+        for z in range(list_square[x]):
+            temporary2.append(temporary1[y + z])
+
+        list_for_push.append(temporary2)
+
+    dic_final.update({'matrix' + str(counter): list_for_push})
+    counter += 1
+
+for x in dic_final.values():
+    for y in x:
+        print(y)
+    print('\n')
