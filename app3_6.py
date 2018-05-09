c1291718d3fb5a562aef062f02fc9052f17e661e
Cupprum <branisa.samuel@windowslive.com>
2018-05-09 20:04:42 +0200

    doesnt know why, but it works

--- app3_6.py
+++ app3_6.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter as tk
+import cmath, math
 
 
 class Rectangle:
@@ -8,22 +9,81 @@ class Rectangle:
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+
         self.canvas = canvas
-        self.ball = canvas.create_rectangle(self.x1, self.y1, self.x2,
-                                            self.y2, fill="red")
+        self.rectangle = canvas.create_rectangle(self.x1, self.y1, self.x2,
+                                                 self.y2, fill="red")
 
 
-root = Tk()
-root.title("Obdlzniky")
-root.resizable(False, False)
+class Polygon:
+    def __init__(self, canvas, x1, y1, x2, y2, alfa):
+        cangle = cmath.exp(math.radians(-alfa) * 1j)
+        center = complex(x1, y2)
 
-canvas = tk.Canvas(root, width=500, height=500, borderwidth=2, relief="solid")
-canvas.grid()
+        v = cangle * (complex(x2, y2) - center) + center
 
-txt = open('../ZADANIA/Zadanie_6_Uloha_1.txt', 'r').readlines()
+        self.x1 = x1
+        self.y1 = y2
+        self.x2 = v.real
+        self.y2 = v.imag
+        self.x3 = 0
+        self.y3 = 0
+        self.x4 = 0
+        self.y4 = 0
 
-for x in txt:
-    temporary = x.split(' ')
+        self.canvas = canvas
+        self.polygon = canvas.create_polygon(self.x1, self.y1,
+                                             self.x2, self.y2,
+                                             self.x3, self.y3,
+                                             self.x4, self.y4,
+                                             fill="red")
+
+
+def everything():
+    root = Tk()
+    root.title("Obdlzniky")
+    root.resizable(False, False)
+
+    canvas = tk.Canvas(root,
+                       width=500,
+                       height=500,
+                       borderwidth=2,
+                       relief="solid")
+    canvas.grid()
+
+    txt = open('../ZADANIA/Zadanie_6_Uloha_1.txt', 'r').readlines()
+
+    for x in txt:
+        temporary = x.split(' ')
+        temporary = list(map(int, temporary))
+
+        x1 = temporary[0]
+        x2 = temporary[2]
+        y1 = temporary[1]
+        y2 = temporary[3]
+
+        rec = Rectangle(canvas, x1, y1, x2, y2)
+        print(rec)
+    root.mainloop()
+
+
+def just_one():
+    number = int(input('set number of Rectangle: '))
+
+    root = Tk()
+    root.title("Obdlznik")
+    root.resizable(False, False)
+
+    canvas = tk.Canvas(root,
+                       width=500,
+                       height=500,
+                       borderwidth=2,
+                       relief="solid")
+    canvas.grid()
+
+    txt = open('../ZADANIA/Zadanie_6_Uloha_1.txt', 'r').readlines()
+
+    temporary = txt[number].split(' ')
     temporary = list(map(int, temporary))
 
     x1 = temporary[0]
@@ -31,6 +91,15 @@ for x in txt:
     y1 = temporary[1]
     y2 = temporary[3]
 
-    rec = Rectangle(canvas, x1, y1, x2, y2)
+    rec1 = Polygon(canvas, x1, y1, x2, y2, 0)
+    rec2 = Polygon(canvas, x1, y1 + 200, x2, y2 + 200, 45)
+    print(rec1, rec2)
+    root.mainloop()
 
-root.mainloop()
+
+what_to_do = input('1 or all: ')
+if what_to_do == '1':
+    just_one()
+
+elif what_to_do == 'all':
+    everything()
