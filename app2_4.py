40e06deb103065c9c5828657646589860444aefd
Cupprum <branisa.samuel@windowslive.com>
2018-05-12 22:10:55 +0200

    app2_4 is done

--- app2_4.py
+++ app2_4.py
@@ -1,1 +1,13 @@
 txt = open('../ZADANIA/VYBER_2/uloha_4.txt', 'r').readlines()
+final = open('final2_4.txt', 'w')
+
+list_temporary = []
+
+for x in txt:
+    list_temporary.append(x[:-1])
+
+for x in list_temporary:
+    help_str = x[:]
+    help_str += x[0]
+    help_str = help_str[1:]
+    final.write(help_str + "\n")
