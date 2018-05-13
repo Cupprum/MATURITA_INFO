8a9968e9860c6bdb8db0ac74f9f75c7773d548d2
Cupprum <branisa.samuel@windowslive.com>
2018-05-14 00:02:09 +0200

    looks promising

--- app1_4.py
+++ app1_4.py
@@ -1,5 +1,5 @@
 # sequence = input('vvjjsskK')
-sequence = '123'
+sequence = '1231'
 list_final = []
 
 '''
@@ -20,9 +20,18 @@ for x in range(counter):
 
         for z in range(counter - 2):
             str_final += list_of_letters[2]
-            list_final.append(str_final)
-            str_final = str_final[:-2]
 
+            for z in range(counter - 3):
+                str_final += list_of_letters[3]
+
+                if str_final not in list_final:
+                    list_final.append(str_final)
+
+            str_final = str_final[:-2]
+            list_of_letters.append(list_of_letters[2])
+            del list_of_letters[2]
+
+        str_final = str_final[:-1]
         list_of_letters.append(list_of_letters[1])
         del list_of_letters[1]
 
@@ -30,31 +39,4 @@ for x in range(counter):
     del list_of_letters[0]
 
 print(list_final)
-
-
-sequence = '123'
-list_final = []
-
-list_of_letters = list(sequence)
-print(list_of_letters)
-
-counter = len(list_of_letters)
-
-for _ in range(counter):
-    str_final = list_of_letters[0]
-
-    for _ in range(counter - 1):
-        str_final += list_of_letters[1]
-
-        for _ in range(counter - 2):
-            str_final += list_of_letters[2]
-            list_final.append(str_final)
-            str_final = str_final[:-2]
-
-        list_of_letters.append(list_of_letters[1])
-        del list_of_letters[1]
-
-    list_of_letters.append(list_of_letters[0])
-    del list_of_letters[0]
-
-print(list_final)
+print(len(list_final))
